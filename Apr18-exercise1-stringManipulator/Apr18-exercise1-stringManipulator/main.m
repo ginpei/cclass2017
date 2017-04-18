0166a1b52fdc5cea3f09766148ac9dfdb68ac381
ginpei <ginpei@gmail.com>
2017-04-18 14:33:34 -0700

    init

---
+++ Apr18-exercise1-stringManipulator/Apr18-exercise1-stringManipulator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Apr18-exercise1-stringManipulator
+//
+//  Created by Ginpei on 2017-04-18.
+//  Copyright © 2017 Ginpei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
