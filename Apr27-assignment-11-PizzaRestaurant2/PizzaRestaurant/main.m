8b64dcc6ae62a3c65269c4fe669e5b1cb485d9cb
ginpei <ginpei@gmail.com>
2017-04-27 18:10:26 -0700

    part2

---
+++ Apr27-assignment-11-PizzaRestaurant2/PizzaRestaurant/main.m
@@ -0,0 +1,56 @@
+//
+//  main.m
+//  PizzaRestaurant
+//
+//  Created by Steven Masuch on 2014-07-19.
+//  Copyright (c) 2014 Lighthouse Labs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "Kitchen.h"
+#import "Pizza.h"
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSLog(@"Please pick your pizza size and toppings:");
+        
+        Kitchen *restaurantKitchen = [Kitchen new];
+        
+        while (TRUE) {
+            // Loop forever
+            
+            NSLog(@"> ");
+            char str[100];
+            fgets (str, 100, stdin);
+            
+            NSString *inputString = [[NSString alloc] initWithUTF8String:str];
+            inputString = [inputString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+            
+            NSLog(@"Input was %@", inputString);
+            
+            // Take the first word of the command as the size, and the rest as the toppings
+            NSArray *commandWords = [inputString componentsSeparatedByString:@" "];
+            NSString *keyword = commandWords[0];
+            
+            Pizza *pizza;
+            if ([keyword isEqualToString:@"pepperoni"]) {
+                pizza = [Pizza largePepperoni];
+            }
+            else {
+                // And then send some message to the kitchen...
+                PizzaSize size = [Pizza sizeFromString:keyword];
+                NSArray *toppings = [commandWords subarrayWithRange:NSMakeRange(1, commandWords.count - 1)];
+                pizza = [[Pizza alloc] initWithSize:size toppings:toppings];
+            }
+            
+            NSLog(@"Here is a %@.", pizza);
+        }
+
+    }
+    return 0;
+}
+
