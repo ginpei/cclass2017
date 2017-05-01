d1659f7359f8dc4b27aae5acbce9156fd23ddb23
ginpei <ginpei@gmail.com>
2017-05-01 16:02:59 -0700

    payments

---
+++ May01-assignment-10-Payments/May01-assignment-10-Payments/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  May01-assignment-10-Payments
+//
+//  Created by Ginpei on 2017-05-01.
+//  Copyright © 2017 Ginpei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
