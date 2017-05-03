f00f836b80698eb522ad09804555b9c4ffc8bc85
ginpei <ginpei@gmail.com>
2017-05-02 17:19:27 -0700

    pig latinize

--- May02-assignment-13-PigLatin/May02-assignment-13-PigLatin/NSString+stringByPigLatinization.m
+++ May02-assignment-13-PigLatin/May02-assignment-13-PigLatin/NSString+stringByPigLatinization.m
@@ -12,7 +12,30 @@
 
 -(NSString *)stringByPigLatinization
 {
-    return @"[pigLatinized]";
+    NSMutableArray *results = [NSMutableArray new];
+    NSArray *words = [self componentsSeparatedByCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    for (int i = 0; i < words.count; i++) {
+        [results addObject:[self pigLatinize:words[i]]];
+    }
+    
+    return [results componentsJoinedByString:@" "];
+}
+
+-(NSString *)pigLatinize: (NSString *)original {
+    NSString *result = NULL;
+    
+    if (original.length > 0) {
+        NSRange range = [original rangeOfCharacterFromSet:[NSCharacterSet characterSetWithCharactersInString:@"aeiou"]];
+        NSLog(@"%lul", (unsigned long)range.location);
+        if (range.location < original.length) {
+            result = [NSString stringWithFormat:@"%@%@ay", [original substringFromIndex:range.location], [original substringToIndex:range.location]];
+        }
+    }
+    
+    if (result == NULL) {
+        result = original;
+    }
+    return result;
 }
 
 @end
